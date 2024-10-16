87f6794c74e27ac3cb95dfe422755b41bc8280d2
endomorphosis <starworks5@gmail.com>
2024-10-16 00:10:07 -0700

    Add @dsnp/parquetjs dependency for IPFS Parquet to CAR conversion

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-export * from './ipfs_datasets_js/ipfs_datasets.js';
-export { ipfsDatasetsJs } from './ipfs_datasets_js/ipfs_datasets.js';
-export { ipfsDatasetsJs as default } from './ipfs_datasets_js/ipfs_datasets.js';
+export * from './ipfs_parquet_to_car_js/ipfs_parquet_to_car.js';
+export { ipfsParquetToCarJs } from './ipfs_parquet_to_car_js/ipfs_parquet_to_car.js';
+export { ipfsParquetToCarJs as default } from './ipfs_parquet_to_car_js/ipfs_parquet_to_car.js';
 export * from './test/test.js';
-export { test_ipfs_datasets_js } from './test/test.js';
-import { test_ipfs_datasets_js } from './test/test.js';
-const test = new test_ipfs_datasets_js();
+export { test_parquet_to_car_js } from './test/test.js';
+import { test_parquet_to_car_js } from './test/test.js';
+const test = new test_parquet_to_car_js();
 export { test };
--- ipfs_parquet_to_car_js/ipfs_parquet_to_car.js
+++ ipfs_parquet_to_car_js/ipfs_parquet_to_car.js
@@ -1,15 +1,42 @@
+import parquetjs from '@dsnp/parquetjs';
+
 export class ipfsParquetToCarJs{
     constructor(resources, metadata){
         this.resources = resources;
         this.metadata = metadata;
     }
 
-    async convert_to_car(){
+    async convert_to_car(parquet_file){
+        try{
+            const parquet = await parquetjs.ParquetReader.openFile(parquet_file);
+            const cursor = parquet.getCursor();
+            let record = null;
+            while (record = await cursor.next()) {
+                console.log(record);
+            }
+        }catch(e){
+            console.log(e);
+        }
 
     }
 
     async convert_to_parquet(){
 
     }
+
+    async test(){
+        console.log("Hello from ipfs_parquet_to_car.js");
+        parquet_file = "bafkreidnskbqrb2uthybtvt7fazaxlzbdemci7acbozcfh2akerz6ujeza.parquet";
+        try{
+            await this.convert_to_car(parquet_file);
+        }
+        catch(e){
+            console.log(e);
+        }
+
+    }
 }
 export default ipfsParquetToCarJs;
+
+const ipfsParquetToCarJs = new ipfsParquetToCarJs();
+ipfsParquetToCarJs.test();
