be400f23d180ed10d8b291cf9af9353b28739b32
endomorphosis <starworks5@gmail.com>
2024-10-15 23:17:49 -0700

    Add ipfsParquetToCarJs class for converting between IPFS Parquet and CAR formats

---
+++ ipfs_parquet_to_car_js/ipfs_parquet_to_car.js
@@ -0,0 +1,15 @@
+export class ipfsParquetToCarJs{
+    constructor(resources, metadata){
+        this.resources = resources;
+        this.metadata = metadata;
+    }
+
+    async convert_to_car(){
+
+    }
+
+    async convert_to_parquet(){
+
+    }
+}
+export default ipfsParquetToCarJs;
